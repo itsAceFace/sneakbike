89eab7341421f14ef4d3bcdb666b767f77684842
James Salvatore <jimmy.c.salvatore@gmail.com>
2020-06-22 12:05:06 -0500

    feat: added digital_ocean_setup for website;

--- site/digital_ocean_setup.sh
+++ site/digital_ocean_setup.sh
@@ -1,3 +1,4 @@
+# Run this script on a clean droplet, ubuntu:18.04
 sudo apt update \
 && sudo apt install -y \
     vim \
@@ -11,11 +12,21 @@ sudo apt update \
 && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
 && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" \
 && sudo apt update \
-&& sudo apt install -y docker-ce \
+&& sudo apt install -y docker-ce nginx \
 && mkdir -p /src \
 && cd /src \
 && git clone https://github.com/jsal13/sneakbike.git \
 && cd sneakbike/site \
 && docker build -t sneakbike . \
-&& docker run -d -p 8080:8081 sneakbike \
+&& docker run -d -p 8000:8080 sneakbike \
+&& sudo echo "
+server {
+  listen        80;
+  listen        [::]:80 default_server;
+  server_name   sneakbikemysteryrace.com;
 
+  location / {
+    proxy_pass  http://localhost:8000;
+  }
+}" | sudo tee /etc/nginx/sites-available/default \
+&& sudo systemctl restart nginx
