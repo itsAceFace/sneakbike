b8cf1b9197882891a5e4a63496839d88f0525b62
James Salvatore <jimmy.c.salvatore@gmail.com>
2020-09-05 02:23:38 -0500

    feat: added resolutions;

--- site/sneakbike/src/views/ResolutionStandards.vue
+++ site/sneakbike/src/views/ResolutionStandards.vue
@@ -5,8 +5,14 @@
         <v-col cols="2" />
         <v-col cols="8">
           <h1>Resolution Standards</h1>
-          <warning-card>Please do not deviate from these resolutions unless you know what you're doing.</warning-card>
-          <p>Below are the standard resolutions for our consoles. These can be used for:</p>
+          <warning-card
+            >Please do not deviate from these resolutions unless you know what
+            you're doing.</warning-card
+          >
+          <p>
+            Below are the standard resolutions for our consoles. These can be
+            used for:
+          </p>
 
           <ul>
             <li>Runner Emulator Display configuration.</li>
@@ -23,8 +29,15 @@
           <br />
           <info-card>
             The
-            <b>Good Starting Size</b> is a nice border size to start with when using Streamelements. You may have to resize it a bit, but it's a good initial size to iterate on.
+            <b>Good Starting Size</b> is a nice border size to start with when
+            using Streamelements. You may have to resize it a bit, but it's a
+            good initial size to iterate on.
           </info-card>
+
+          <warning-card
+            >N64 games tend to have different resolutions, these should probably
+            be looked at individually.</warning-card
+          >
         </v-col>
       </v-row>
     </v-container>
@@ -54,9 +67,10 @@ const headers = [
 ];
 
 const rows = [
-  { console: "NES", resolution: "TODO", goodStartingSize: "TODO" },
+  { console: "NES", resolution: "16:15", goodStartingSize: "512x480" },
   { console: "SNES", resolution: "8:7", goodStartingSize: "544x476" },
-  { console: "Genesis", resolution: "TODO", goodStartingSize: "TODO" },
+  { console: "Genesis", resolution: "10:7", goodStartingSize: "640x448" },
+  { console: "N64", resolution: "17:12*", goodStartingSize: "680x480*" },
 ];
 
 export default {
